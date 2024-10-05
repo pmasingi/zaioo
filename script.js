cd1414dbd46a2f2c4770428a8eaa7dc779950970
pmasingi <141011027+pmasingi@users.noreply.github.com>
2024-10-05 10:14:58 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('changeTextBtn').addEventListener('click', function() {
+  document.getElementById('dynamicText').textContent = 'This text has been changed dynamically!';
+});
